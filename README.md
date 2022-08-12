bc1c0016bb8fa90745c2744e7e08e98c5bb60a43
Egon Alves <egon.alves@outlook.com.br>
2022-08-11 23:40:09 -0300

    finalizado

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ Código com a função de pesquisar o usuário e listar o repositório dele via 
 
 Nesse projeto eu consegui entender com mais clareza como uma API se conecta, como ela coleta as informações da API, os benefícios de fatorar um código, facilitando na manutenção do projeto e implementação de novos recursos.
 Ao longo do projeto utilizei github para versionamento do código, criando branchs pra separa o antes e o depois do código fatorado.
-Pesquisas a serem feitas.
+
+***Pesquisas a serem feitas.
 -${} podem ser usado direto no html ou precisa injetar com .innerHTML
 - Como fatorar o código de uma maneira eficientes.
 -Uso do async em conjunto com await.
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,9 @@
-import { getUser } from '/src/scripts/services/user.js'
-import { getRepositories } from '/src/scripts/services/repositories.js'
-import { user } from '/src/scripts/objects/user.js'
-import { screen } from '/src/scripts/objects/screen.js'
+console.log('caiuu aqui')
+
+import { getUser } from './services/user.js'
+import { getRepositories } from './services/repositories.js'
+import { user } from './objects/user.js'
+import { screen } from './objects/screen.js'
 // Esse document chama a funcao ao clicar no botao buscar
 document.getElementById("btn-search").addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
